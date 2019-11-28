71e9b9bd9c3948292a154866565901b7f1a58f48
Kevin Rangel <code@venikx.com>
2019-11-29 01:18:27 +0200

    Use gridsome AKA Gatsby for Vue for the build step

---
+++ src/layouts/Default.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="layout">
+    <nav>
+      <ul>
+        <!-- <li><g-link to="/">{{ $static.metadata.siteName }}</g-link></li> -->
+        <li><g-link to="/cv">CV</g-link></li>
+        <li><g-link to="/labs">Lab</g-link></li>
+        <li>
+          <g-link to="/home">
+            <g-image src="~/assets/phoenix.gif" alt="A flying phoenix" />
+          </g-link>
+        </li>
+        <li><g-link to="/blog">Blog</g-link></li>
+        <li><g-link to="/about">About</g-link></li>
+      </ul>
+    </nav>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-family: 'Roboto';
+  background: url("~@/assets/stars.gif") #00001a;
+  }
+
+body::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: darkblue;
+  opacity: 0.1;
+  z-index: -1;
+}
+
+nav {
+  color: white;
+}
+
+nav::after {
+  content: '';
+  background: url("~@/assets/divider-rainbow.gif") #8c1eff;
+  height: 1px;
+  width: 100%;
+  display: inline-block;
+}
+
+a {
+  cursor: auto;
+  cursor: url("~@/assets/hand.gif"), auto;
+  color: white;
+  font-size: 2rem;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <section>
+      <g-image src="~/assets/construction.gif" alt="A GeoCities 'Under Construction' gif" />
+      <g-image src="~/assets/bartxxx.gif" alt="Bart Simpson pulls down pants and moons you" />
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+</style>
