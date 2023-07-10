dbec8518ce6d8142fde8b1ab7d5e1c9afc79d3c7
Kevin Rangel <code@venikx.com>
2023-07-11 01:01:56 +0200

    feat(astro): scuffed minimal org-mode setup

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
 import prefetch from '@astrojs/prefetch'
+import org from 'astro-org'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://venikx.com',
   integrations: [
+    org({}),
     prefetch(),
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
