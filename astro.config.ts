4017079a615a5e9ad244bf7aaf510074fbd21739
Kevin Rangel <code@venikx.com>
2023-07-10 16:05:54 +0200

    feat(astro): add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config'
-
 import image from '@astrojs/image'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://venikx.com',
+
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),
+    sitemap(),
   ],
 })
