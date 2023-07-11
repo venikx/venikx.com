3e1ca726cbb654e69138a8f1d5cc0c94faabcffd
Kevin Rangel <code@venikx.com>
2023-07-11 23:36:02 +0200

    feat(rss): generate rss feed from org files

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+const pages = import.meta.glob<true, '', any>(['./pages/posts/*.org'], {
+  eager: true,
+})
+
+export const allPages = Object.values(pages).filter(
+  (p) => process.env.NODE_ENV === 'development' || !('draft' in p.frontmatter)
+)
