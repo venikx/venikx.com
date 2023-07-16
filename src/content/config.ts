34c7d5d8208a51d3e49c91d8664b63e7b917637d
Kevin Rangel <code@venikx.com>
2023-07-16 15:22:32 +0200

    feat(styling): draft first version of the styled site

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,25 @@
 import { z, defineCollection } from 'astro:content'
 
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    //created: z.string(),
+    //description: z.string(),
+    //cover: z.string(),
+  }),
+})
+
 const authors = defineCollection({
   type: 'data',
   schema: z.object({
     name: z.string(),
+    socials: z.object({
+      email: z.string(),
+      github: z.string(),
+      linkedin: z.string(),
+    }),
   }),
 })
 
-export const collections = { authors }
+export const collections = { blog, authors }
