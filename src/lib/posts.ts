d4c163a41dfede87860232613768b7cecd4990d8
Kevin Rangel <code@venikx.com>
2024-04-19 12:39:14 +0200

    feat: sort posts by date

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -4,7 +4,7 @@ export async function getAllPosts() {
   const allBlogPosts = await getCollection('blog')
   const allInteractivePosts = await getCollection('interactive')
 
-  return [...allBlogPosts, ...allInteractivePosts].filter(
-    (p) => process.env.NODE_ENV === 'development' || !p.data.draft
-  )
+  return [...allBlogPosts, ...allInteractivePosts]
+    .filter((p) => process.env.NODE_ENV === 'development' || !p.data.draft)
+    .sort((a, b) => b.data.created.getTime() - a.data.created.getTime())
 }
