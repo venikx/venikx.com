14a819c9b665cb3ecb802763fa95d7ee5f3c791c
Kevin De Baerdemaeker <code@venikx.com>
2025-09-23 01:48:58 +0300

    chore: upgrade flake

--- flake.nix
+++ flake.nix
@@ -2,21 +2,30 @@
   description = "venikx.com";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = inputs.nixpkgs.legacyPackages.${system};
-      in {
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ nodejs_20 ];
+          packages = with pkgs; [
+            nodejs_24
+          ];
 
-          shellHook = with pkgs; ''
+          shellHook = ''
             export PATH="$PWD/node_modules/.bin/:$PATH"
-            echo "node `${nodejs_20}/bin/node --version`"
           '';
         };
-      });
+      }
+    );
 }
